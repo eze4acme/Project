796e64f59bd704c901313420e620fe5776200a47
Eze Onyebuchi <eze4acme@gmail.com>
2023-07-02 10:00:11 +0100

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import products from './data'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ const [data, setData] = useState(products)
+function handleClick() {
+  setData([])
+}
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {console.log('hi')}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <>
+    {
+      data.map(product => {
+        const {id, name, image, price, description} = product
+        return(
+          <div className="container" key={id}>
+            <h1>{name} <span>X</span> </h1>
+            <img src={image} alt="" width={'150px'} height={'150px'} />
+            <h3>{description}</h3>
+            <h5>{price}</h5>
+            <button onClick={handleClick}>Clear All</button>
+          </div>
+        )
+      })
+    }
+   </>
   )
 }
 
---
+++ src/data.jsx
@@ -0,0 +1,34 @@
+const products = [
+  {
+    id: 1,
+    name: "Product 1",
+    image:
+      "https://freerangestock.com/thumbnail/87871/streets-of-the-heart.jpg",
+    price: 19.99,
+    description: "This is product 1 description.",
+  },
+  {
+    id: 2,
+    name: "Product 2",
+    image: "https://freerangestock.com/thumbnail/40611/desktop-computer.jpg",
+    price: 29.99,
+    description: "This is product 2 description.",
+  },
+  {
+    id: 3,
+    name: "Product 3",
+    image:
+      "https://media.istockphoto.com/id/584574708/photo/soap-bar-and-liquid-shampoo-shower-gel-towels-spa-kit.jpg?s=612x612&w=0&k=20&c=TFeQmTwVUwKY0NDKFFORe3cwDCxRtotFgEujMswn3dc=",
+    price: 39.99,
+    description: "This is product 3 description.",
+  },
+  {
+    id: 4,
+    name: "Product 4",
+    image: "https://freerangestock.com/thumbnail/27297/idli-and-vada.jpg",
+    price: 49.99,
+    description: "This is product 4 description.",
+  },
+];
+
+export default products
