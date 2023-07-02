8cdfdd5b8b4df881762d79930e0efbd6f5811da8
Eze Onyebuchi <eze4acme@gmail.com>
2023-07-03 00:24:10 +0100

    update app.jsx

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log('hi');
+
+const arr = ['bob', 'susy', 'bob' , 'luke']
+
+console.log([...new Set(arr)]);
--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,80 @@
-import { useState } from 'react'
 import products from './data'
 import './App.css'
 
+// function App() {
+//     console.log("removedItem");
+//  const [data, setData] = useState(products)
+// function removeItem(id) {
+//   setData(prevData => {
+//     return prevData.filter((product) => {
+  //       return product.id !== id;
+//     })
+//   })
+
+// }
+// function handleClick() {
+  //   setData([])
+  // }
+  //   return (
+//     <>
+//       <div className="container" >
+//         {data.map((product) => {
+//           const { id, name, image, price, description } = product;
+//           return (
+//             <div
+//               className="sub-container"
+//               key={id}
+//               style={{ display: "grid", gridTemplate: "auto/1fr 1fr 1fr" }}
+//             >
+//               <h1>
+//                 {name}{" "}
+//                 <span style={{ color: "red" }} onClick={() => removeItem(id)}>
+//                   X
+//                 </span>
+//               </h1>
+//               <img
+//                 src={image}
+//                 alt=""
+//                 width={"150px"}
+//                 height={"150px"}
+//                 style={{ objectFit: "cover" }}
+//               />
+//               <h3>{description}</h3>
+//               <h5>{price}</h5>
+//             </div>
+//           );
+//         })}
+//         <button onClick={handleClick} style={{backgroundColor:'purple'}}>Clear All</button>
+//       </div>
+//     </>
+//   );
+// }
+
+import React from "react";
+import { useState, useEffect } from 'react'
+
 function App() {
- const [data, setData] = useState(products)
-function removeItem(id) {
-  setData(prevData => {
-    return prevData.filter((product) => {
-      return product.id !== id;
-    })
-  })
-  // console.log(removedItem);
-}
-function handleClick() {
-  setData([])
-}
+  const [size, setSize] = useState(window.innerWidth)
+
+  function checkSize() {
+    setSize( window.innerWidth)
+  }
+  
+useEffect(() =>{
+  window.addEventListener('resize', checkSize)
+  console.log('call useeffect');
+
+  return () => {
+    window.removeEventListener('resize', checkSize)
+    console.log('remove');
+  }
+})
+
   return (
-    <>
-      <div className="container" >
-        {data.map((product) => {
-          const { id, name, image, price, description } = product;
-          return (
-            <div
-              className="sub-container"
-              key={id}
-              style={{ display: "grid", gridTemplate: "auto/1fr 1fr 1fr" }}
-            >
-              <h1>
-                {name}{" "}
-                <span style={{ color: "red" }} onClick={() => removeItem(id)}>
-                  X
-                </span>
-              </h1>
-              <img
-                src={image}
-                alt=""
-                width={"150px"}
-                height={"150px"}
-                style={{ objectFit: "cover" }}
-              />
-              <h3>{description}</h3>
-              <h5>{price}</h5>
-            </div>
-          );
-        })}
-        <button onClick={handleClick} style={{backgroundColor:'purple'}}>Clear All</button>
-      </div>
-    </>
-  );
+    <div>
+        <h1>{size}</h1>
+    </div>
+  )
 }
 
 export default App
