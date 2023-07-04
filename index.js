a84ff48b79edb67a79d61213bdf47e1b41839aed
Eze Onyebuchi <eze4acme@gmail.com>
2023-07-04 18:18:13 +0100

    Update App.jsx
    completed a simple project of show and hide item using useState and useEffect and also applying clean up function.

--- index.js
+++
@@ -1,5 +0,0 @@
-console.log('hi');
-
-const arr = ['bob', 'susy', 'bob' , 'luke']
-
-console.log([...new Set(arr)]);
--- src/App.jsx
+++ src/App.jsx
@@ -1,130 +1,5 @@
-import products from './data'
+
 import './App.css'
-
-// function App() {
-//     console.log("removedItem");
-//  const [data, setData] = useState(products)
-// function removeItem(id) {
-//   setData(prevData => {
-//     return prevData.filter((product) => {
-  //       return product.id !== id;
-//     })
-//   })
-
-// }
-// function handleClick() {
-  //   setData([])
-  // }
-  //   return (
-//     <>
-//       <div className="container" >
-//         {data.map((product) => {
-//           const { id, name, image, price, description } = product;
-//           return (
-//             <div
-//               className="sub-container"
-//               key={id}
-//               style={{ display: "grid", gridTemplate: "auto/1fr 1fr 1fr" }}
-//             >
-//               <h1>
-//                 {name}{" "}
-//                 <span style={{ color: "red" }} onClick={() => removeItem(id)}>
-//                   X
-//                 </span>
-//               </h1>
-//               <img
-//                 src={image}
-//                 alt=""
-//                 width={"150px"}
-//                 height={"150px"}
-//                 style={{ objectFit: "cover" }}
-//               />
-//               <h3>{description}</h3>
-//               <h5>{price}</h5>
-//             </div>
-//           );
-//         })}
-//         <button onClick={handleClick} style={{backgroundColor:'purple'}}>Clear All</button>
-//       </div>
-//     </>
-//   );
-// }
-
-// import React from "react";
-
-// function App() {
-//   const [size, setSize] = useState(window.innerWidth)
-
-//   function checkSize() {
-  //     setSize( window.innerWidth)
-//   }
-
-// useEffect(() =>{
-  //   window.addEventListener('resize', checkSize)
-  //   console.log('call useeffect');
-  
-  //   return () => {
-//     window.removeEventListener('resize', checkSize)
-//     console.log('remove');
-//   }
-// })
-
-//   return (
-  //     <div>
-  //         <h1>{size}</h1>
-  //     </div>
-  //   )
-  // }
-
-  // export default App
-  
-// import { useState, useEffect } from 'react'
-// import React from 'react'
-
-// function App() {
-// const [isLoading, setIsLoading] = useState(true)
-// const [isError, setIsError] = useState(false)
-// const [value, setValue] = useState([])
-// useEffect(() =>{
-// fetch("https://api.github.com/users")
-// .then(res => {
-//   if (res.ok) {
-//     return res.json()
-//   }else{
-//     setIsLoading(false)
-//     setIsError(true)
-//     throw new Error(res.statusText)
-//   }
-// })
-// .then(data => {
-//   // const {login} = data
-//   console.log(data[0].login);
-//   setValue(data[0].login);
-//   setIsLoading(false)
-// })
-// .catch(error => console.error(error))
-// }, [])
-//  if(isLoading){
-//    return (
-//      <div>
-//       <h1>Loading....</h1>
-//      </div>
-//    );
-//  }
-//  if(isError){
-//    return (
-//      <div>
-//        <h1>Error.....</h1>
-//      </div>
-//    );
-//  }
-//  return (
-//   <h1>{value}</h1>
-//  )
-//   }
-
-// export default App
-
 import React, { useState, useEffect } from 'react'
 
 function App() {
@@ -143,10 +18,19 @@ function App() {
 }
 
 function Item() {
-  
+  const [resize, setResize] = useState(window.innerWidth);
+  function checkSize() {
+    setResize(window.innerWidth)
+  }
+  useEffect(() =>{
+    window.addEventListener('resize', checkSize)
+    return () =>{
+      window.removeEventListener('resize', checkSize)
+    }
+  }, [])
  return (<div>
   <h1>Window</h1>
-  <h2>Size:</h2>
+  <h2>Size: {resize}px</h2>
   </div>)
 }
 
